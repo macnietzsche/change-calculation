46efa055ef14e3fb904be318c11a81b63df2386f
macnietzsche <mbvillegas94@outlook.com>
2021-11-07 14:48:34 -0500

    REFACTOR: TDD Green Has enough change

--- src/services/change_calculation.py
+++ src/services/change_calculation.py
@@ -1,2 +1,14 @@
-def has_enough_change(payments_received):
-    return None
+from typing import List
+
+TICKET_COST =  25
+INITIAL_CASH = 0
+POSITIVE_RESPONSE = "SI"
+NEGATIVE_RESPONSE = "NO"
+
+def has_enough_change(payments_received: List[int]) -> str:
+    current_cash = INITIAL_CASH
+    for payment in payments_received:
+        current_cash += 2 * TICKET_COST - payment
+        if current_cash <= 0:
+            return NEGATIVE_RESPONSE
+    return POSITIVE_RESPONSE
