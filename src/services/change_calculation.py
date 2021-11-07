29e7b99c2d957123aa88ae58e55e8ae19cde7556
macnietzsche <mbvillegas94@outlook.com>
2021-11-07 17:45:00 -0500

    REFACTOR: TDD Red. TDD green on dot_product

--- src/services/change_calculation.py
+++ src/services/change_calculation.py
@@ -1,19 +1,29 @@
+from functools import reduce
 from typing import List
-
+from itertools import product
 TICKET_COST =  25
-INITIAL_CASH = 0
 POSITIVE_RESPONSE = "SI"
 NEGATIVE_RESPONSE = "NO"
-VALID_INPUTS = [25, 50, 100]
+VALID_INPUTS = {25: 0, 50: 0, 100: 0}
+
+def dot_product(vector_a: List[float], vector_b: List[float])->float:
+   if len(vector_a) != len(vector_b):
+      return 0
+   return sum(i[0] * i[1] for i in zip(vector_a, vector_b))
+
+def process_change(required_value: int, current_cash: dict):
+    return None
 
 def has_enough_change(payments_received: List[int]) -> str:
-    current_cash = INITIAL_CASH
+    current_cash = VALID_INPUTS
     for payment in payments_received:
-        if not payment in VALID_INPUTS:
+        if not payment in VALID_INPUTS.keys():
             raise Exception("Input is not valid")
-
-        current_cash += 2 * TICKET_COST - payment
-        if current_cash <= 0:
+        
+        change = payment - TICKET_COST
+        current_cash = process_change(change,current_cash)
+        if not current_cash:
             return NEGATIVE_RESPONSE
-            
+        current_cash[payment] += 1
+    
     return POSITIVE_RESPONSE
--- test/test_change_calculation.py
+++ test/test_change_calculation.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-from services.change_calculation import has_enough_change
+from services.change_calculation import has_enough_change,dot_product, process_change
 
 class TestChangeCalculation(TestCase):
     def test_change_calculation(self):
@@ -38,3 +38,83 @@ class TestChangeCalculation(TestCase):
         payments_received = [25, "a", "h", 100]
         with self.assertRaises(Exception):
             has_enough_change(payments_received)
+
+    def test_dot_product(self):
+        array_a = [1, 2, 3]
+        arrab_b = [1, 2, 3]
+        expected_result = 14
+        result = dot_product(array_a, arrab_b)
+        self.assertEqual(expected_result,result)
+
+        array_a = [0, 2, 4, 6]
+        arrab_b = [1, 4, 6, 90]
+        expected_result = 572
+        result = dot_product(array_a, arrab_b)
+        self.assertEqual(expected_result,result)
+
+        array_a = [-5, 7, 3, -5, -56]
+        arrab_b = [1, 25, 13, 0, -5]
+        expected_result = 489
+        result = dot_product(array_a, arrab_b)
+        self.assertEqual(expected_result,result)
+
+    def test_process_change(self):
+        # Case 1
+        current_bills = {25: 0, 50: 0, 100: 0}
+        change = 0
+
+        values = list(current_bills.values())
+        keys = list(current_bills.keys())
+        
+        expected_result = dot_product(values,keys) - change
+        
+        processed_change = process_change(change, current_bills)
+
+        processed_values = list(processed_change.values())
+        processed_keys = list(processed_change.keys())
+
+        result = dot_product(processed_values,processed_keys)
+
+        self.assertEqual(expected_result,result)
+
+        # Case 2
+        current_bills = {25: 6, 50: 1, 100: 0}
+        change = 100
+
+        values = list(current_bills.values())
+        keys = list(current_bills.keys())
+        
+        expected_result = dot_product(values,keys) - change
+        
+        processed_change = process_change(change, current_bills)
+
+        processed_values = list(processed_change.values())
+        processed_keys = list(processed_change.keys())
+
+        result = dot_product(processed_values,processed_keys)
+
+        self.assertEqual(expected_result,result)
+
+        # Case 3
+        current_bills = {25: 3, 50: 0, 100: 0}
+        change = 100
+
+        expected_result = None
+        result = process_change(change, current_bills)
+        
+        self.assertEqual(expected_result,result)
+
+        # Case 4
+        current_bills = {25: 0, 50: 2, 100: 1}
+        change = 175
+
+        expected_result = None
+        result = process_change(change, current_bills)
+        
+        self.assertEqual(expected_result,result)
+
+
+
+
+
+
